381a1b9fcd361d5fb698917c72510d8fb0c295b7
uttaraonline <uttaraonline@CHAMPAK-LAP>
2022-08-27 12:37:43 +0530

    push alll new

---
+++ vinay/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vinay.Models;
+
+namespace vinay.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult createPost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult createPost(MyPostCollection collections)
+        {
+
+            string filename = Path.GetFileNameWithoutExtension(collections.PostImage.FileName);
+            string extention = Path.GetExtension(collections.PostImage.FileName);
+            filename = filename + DateTime.Now.ToString("yymmssff") + extention;
+            collections.PostImagePath = "/Content/ImagesByUser/" + filename;
+            filename = Path.Combine(Server.MapPath("/Content/ImagesByUser/"), filename);
+            collections.PostImage.SaveAs(filename);
+            try
+            {
+                SqlParameter postname = new SqlParameter();
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Message = ex.ToString();
+            }
+                return View();
+        }
+    }
+}
---
+++ vinay/Models/MyPostCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vinay.Models
+{
+    public class MyPostCollection
+    {
+        public string PostDescription { get; set; }
+        public HttpPostedFileBase PostImage { get; set; }
+        public string PostImagePath { get; set; }
+        public HttpPostedFileBase PostImage1 { get; set; }
+        public HttpPostedFileBase PostImage2 { get; set; }
+        public HttpPostedFileBase PostVideo { get; set; }
+        public string PostLat { get; set; }
+        public string PostLng { get; set; }
+
+    }
+}
